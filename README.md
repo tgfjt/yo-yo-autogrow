6059be654d3b20a2af605a618b7a71d4b5c98e61
tgfjt <tgfjt.mail@gmail.com>
2016-08-25 13:29:16 +0900

    add example autogrow module with choo

--- README.md
+++ README.md
@@ -1,3 +1,13 @@
 # autogrow
 
 wip
+
+## Usage
+
+```js
+var autogrow = require('yo-yo-autogrow')
+
+var el = yo`<div>
+  ${autogrow()}
+</div>`
+```
---
+++ example/sample.js
@@ -0,0 +1,41 @@
+const choo = require('choo')
+const html = require('choo/html')
+const sf = require('sheetify')
+
+const autogrow = require('..')
+
+const prefix = sf('./sample.css')
+
+const app = choo()
+
+app.model({
+  state: {title: 'Autgrow Textarea!'},
+  reducers: {
+    update: (data, state) => ({ title: data })
+  }
+})
+
+const mainView = (state, prev, send) => html`
+  <main class="${prefix}">
+    <div class="container">
+      <h1>${state.title}</h1>
+      <h2>Default</h2>
+      <div>
+        ${autogrow()}
+      </div>
+      <h3>Custom</h3>
+      <div class="custom">
+        ${autogrow({
+          placeholder: 'Placeholder from choo!'
+        })}
+      </div>
+    </div>
+  </main>
+`
+
+app.router((route) => [
+  route('/', mainView)
+])
+
+const tree = app.start()
+document.body.appendChild(tree)
