4a49a859b2ebcaa4bcd35519d71adc97ee0c348a
tgfjt <tgfjt.mail@gmail.com>
2016-08-25 13:28:42 +0900

    use sheetify for default style

--- index.js
+++ index.js
@@ -1,8 +1,51 @@
 var yo = require('yo-yo')
+var sf = require('sheetify')
 
-// todo
+var prefix = sf('./index.css')
 
-var autogrow = null;
+module.exports = function(options) {
+  var value = ' '
+  var ph = 'autogrow';
 
-module.exports = autogrow
+  if (options) {
+    if (options.placeholder) ph = options.placeholder
+    if (options.value) value = options.value + value
+  }
 
+  function textarea (isFocus, val, oninput, placeholder) {
+    placeholder = placeholder || ph
+    val = val || value
+    var tVal = val.trim()
+    var focusClass = isFocus ? 'is-focus' : ''
+
+    return yo`
+      <div class="${prefix}">
+        <div class="AutogrowTextarea ${focusClass}">
+          <div class="AutogrowTextarea-mirror">${val}</div>
+          <div class="AutogrowTextarea-container">
+            <textarea placeholder="${placeholder}" rows="1" autocomplete="off"
+              onfocus=${focus}
+              onblur=${blur}
+              oninput=${oninput}>${tVal}</textarea>
+          </div>
+        </div>
+      </div>
+    `
+  }
+
+  var el = textarea(false, value, update)
+
+  function update (e) {
+    yo.update(el, textarea(true, e.target.value + ' ', update))
+  }
+
+  function focus (e) {
+    yo.update(el, textarea(true, e.target.value + ' ', update))
+  }
+
+  function blur (e) {
+    yo.update(el, textarea(false, e.target.value + ' ', update))
+  }
+
+  return el
+}
